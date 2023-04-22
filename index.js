bc9fb4e735b2b6593d9af4cad565c1a8e07991c4
Pappu Dey <pappudey.coder@gmail.com>
2023-04-22 20:14:05 +0600

    track your express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('This is home page!');
+})
+
+app.listen(3000, ()=>{
+    console.log('listening port 3000');
+})
+
+
+
