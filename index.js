63e25840e1454e688b2855953d77d6d8b9adcf7a
Pappu Dey <pappudey.coder@gmail.com>
2023-04-22 21:56:43 +0600

    express complete

--- index.js
+++ index.js
@@ -2,8 +2,31 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res)=>{
+// app.use(express.json());
+
+// app.use(express.raw());
+// app.use(express.text());
+
+// access static folder 
+
+// app.use(express.static(`${__dirname}/public/`, {
+//     index : "home.html"
+// }))
+
+const router = express.Router({
+    caseSensitive : true,
+});
+
+app.use(router);
+
+router.get('/About', (req, res)=>{
     res.send('This is home page!');
+})
+
+app.post('/', (req, res)=>{
+
+    res.send('This is home page with post method');
+
 })
 
 app.listen(3000, ()=>{
